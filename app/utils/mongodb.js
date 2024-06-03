ce16b7db4452bfbddeb7704c7b81f1e5ff069eb7
Pradip Sable <sablepradipsubhash@gmail.com>
2024-06-03 14:51:19 +0530

    changes in mongodb collections

--- app/utils/mongodb.js
+++ app/utils/mongodb.js
@@ -8,14 +8,15 @@ const connectToDatabase = async () => {
   try {
     if (connection.isConnected) {
       console.log('Using existing database connection');
-      return;
+      return connection.db; // Return the MongoDB client instance
     }
 
     const db = await mongoose.connect(process.env.MONGODB_URI);
 
     connection.isConnected = db.connections[0].readyState;
+    connection.db = db; // Store the MongoDB client instance
     console.log('MongoDB Connected');
-    return db
+    return db;
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     throw new Error('Database connection error');
